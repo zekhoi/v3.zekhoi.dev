ce9ccf4a5e5d920e1df7b4cadcd31d75f27a1de2
Khoironi Kurnia Syah <55708473+zekhoi@users.noreply.github.com>
2023-08-31 22:52:19 +0800

    chore: update latest work experience

--- data/experiences.js
+++ data/experiences.js
@@ -1,7 +1,14 @@
 module.exports = [
   {
-    startTime: "May 2023",
+    startTime: "August 2023",
     endTime: "Now",
+    role: "Product Engineer (Backend - Node.js)",
+    company: "Zero One Group",
+    description: "Zero One Technology",
+  },
+  {
+    startTime: "May 2023",
+    endTime: "August 2023",
     role: "Product Engineer Intern (Backend - Node.js)",
     company: "Zero One Group",
     description: "ZOG Summer Internship Program 2023",
